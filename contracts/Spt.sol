012f731f5e1db4fc9318ee7615c65a5102a86cd8
Andrey Sobol <andreysobol@protonmail.com>
2021-09-17 22:12:37 +0300

    getRoot

--- contracts/Spt.sol
+++ contracts/Spt.sol
@@ -8,9 +8,19 @@ contract Spt {
     uint internal depth;
     uint internal maxElements;
 
+    mapping(uint256 => mapping(uint256 => bytes32)) internal lists;
+
     function setupDepth(uint _depth) public {
         depth = _depth;
         maxElements = 2**depth;
     }
 
+    function getRoot() public view returns (uint) {
+        if (lists[depth][0] == 0x00) {
+            return 0;
+            //calculate_empty_leaf_hash will be here
+        } else {
+            return lists[depth][0];
+        }
+    }
 }
