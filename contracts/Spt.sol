fd2535e6ee705909469e7ad7d973befdfa2c538a
Andrey Sobol <andreysobol@protonmail.com>
2021-09-17 20:00:41 +0300

    2 inited els

--- contracts/Spt.sol
+++ contracts/Spt.sol
@@ -2,4 +2,8 @@ pragma solidity >=0.8.0;
 
 /// @title Voting with delegation.
 contract Spt {
+
+    bytes32 internal emptyElement = 0x0000000000000000000000000000000000000000000000000000000000000000;
+    mapping(uint256 => bytes32) internal cacheEmptyValues;
+
 }
