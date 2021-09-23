1023cb1a50ec1ae2514084ba8e6d96c62386d5ac
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2021-09-23 17:09:07 +0300

    Add empty_roots test

--- tests/test_spt.py
+++ tests/test_spt.py
@@ -9,3 +9,12 @@ def test_setup_depth(public_spt, accounts):
     empty_hash = hashlib.sha256(b'').digest()
     empty_root = '0x' + hashlib.sha256(empty_hash * 2).hexdigest()
     assert root == empty_root
+
+def test_empty_roots(public_spt, accounts):
+    check_size = range(1, 15)
+    trees = { i: PublicSpt.deploy(i, {"from": accounts[0]}) for i in check_size }
+    empty_hash = hashlib.sha256(b'').digest()
+    for i in check_size:
+        empty_hash = hashlib.sha256(empty_hash * 2).digest()
+        assert trees[i].getRoot() == '0x' + empty_hash.hex()
+
