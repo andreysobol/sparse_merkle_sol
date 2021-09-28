d8d1068cf3168b82c46ba3f390946fd84b363d92
Andrey Sobol <andreysobol@protonmail.com>
2021-09-28 13:51:41 +0300

    keccak

---
+++ contracts/PublicKeccak256Spt.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "./Keccak256Spt.sol";
+
+contract PublicSpt is Keccak256Spt {
+    constructor(uint _depth) Keccak256Spt(_depth) public {}
+
+    function _increaseDepth(uint depthDifference) public {
+        super.increaseDepth(depthDifference);
+    }
+
+    function _decreaseDepth(uint depthDifference) public {
+        super.decreaseDepth(depthDifference);
+    }
+
+    function _modifyElement(uint index, bytes calldata element) public {
+        super.modifyElement(index, element);
+    }
+
+    function _addElement(uint index, bytes calldata element) public {
+        super.addElement(index, element);
+    }
+
+    function _removeElement(uint index) public {
+        super.removeElement(index);
+    }
+}
---
+++ scripts/keccak_gas.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from brownie import PublicKeccak256Spt, accounts
+
+def main():
+    tree = PublicKeccak256Spt.deploy(10, {"from": accounts[0]})
+
+    gas_used_add = []
+    for item in range(0, 1024):
+        tx = tree._addElement(item, b'apple')
+        gas_used_add.append(tx.gas_used)
+
+    gas_used_remove = []
+    for item in range(0, 1024):
+        tx = tree._removeElement(item)
+        gas_used_remove.append(tx.gas_used)
+
+    print("ADD")
+    print("Median: ")
+    print(sorted(gas_used_add)[512])
+    print("Average: ")
+    print(sum(gas_used_add)/1024)
+    print("Min: ")
+    print(min(gas_used_add))
+    print("Max: ")
+    print(max(gas_used_add))
+    print("")
+    print("REMOVE")
+    print("Median: ")
+    print(sorted(gas_used_remove)[512])
+    print("Average: ")
+    print(sum(gas_used_remove)/1024)
+    print("Min: ")
+    print(min(gas_used_remove))
+    print("Max: ")
+    print(max(gas_used_remove))
+
+    kv = {
+        "gas_used_add": gas_used_add,
+        "gas_used_remove": gas_used_remove,
+    }
+
+    import json
+    with open('raw_data_keccak.json', 'w') as f:
+        json.dump(kv, f)
