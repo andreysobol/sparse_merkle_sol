2c8eda5afa19b06aa3b6dce200196cbdaa1157a9
Andrey Sobol <andreysobol@protonmail.com>
2021-12-13 17:24:50 +0200

    create_plot_set_sha

--- diagram.py
+++ diagram.py
@@ -105,6 +105,49 @@ def create_plot2(
     plt.savefig('plot2.png')
     plt.show()
 
+def create_plot_set_sha(
+    gas_used_add_sha,
+    gas_used_remove_last_sha,
+    gas_used_remove_random_sha,
+):
+
+    xdata1 = [item for item in range(0, len(gas_used_add_sha))]
+    xdata2 = [item for item in range(0, len(gas_used_remove_last_sha))]
+    xdata3 = [item for item in range(0, len(gas_used_remove_random_sha))]
+
+    # create some y data points
+    ydata1 = gas_used_add_sha
+    ydata2 = gas_used_remove_last_sha
+    ydata3 = gas_used_remove_random_sha
+
+    # plot the data
+    fig = plt.figure()
+    fig.set_size_inches(10, 7)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_ylabel("gas")
+    ax.set_xlabel("update index")
+
+
+    ax.plot(xdata1, ydata1, color='red', marker='o', markersize=1, linewidth = 0.0)
+    ax.plot(xdata2, ydata2, color='green', marker='o', markersize=1, linewidth = 0.0)
+    ax.plot(xdata3, ydata3, color='blue', marker='o', markersize=1, linewidth = 0.0)
+
+    ax.legend([
+        'Sha256 add element',
+        'Sha256 remove last',
+        'Sha256 remove random',
+    ])
+
+    # set the limits
+    ax.set_xlim([0, 1024])
+    ax.set_ylim([0, 400000])
+
+    ax.set_title('Gas usage')
+
+    # display the plot
+    plt.savefig('plot_set_sha.png')
+    plt.show()
+
 import json
 
 def read_from_file_and_run():
@@ -126,5 +169,13 @@ def read_from_file_and_run():
                 data_keccak["gas_used_remove_last"],
                 data_keccak["gas_used_remove_random"],
             )
+
+    with open('raw_data_set_sha.json') as json_sha_file:
+        data_sha = json.load(json_sha_file)
+        create_plot_set_sha(
+            data_sha["gas_used_add"],
+            data_sha["gas_used_remove_last"],
+            data_sha["gas_used_remove_random"],
+        )
 
 read_from_file_and_run()
